082b76def09b32fe63144667bc36ddc3fe3b4acf
Jennifer Weir <jennweir@umich.edu>
2024-10-14 22:19:07 -0500

    following docs

--- hello/go.mod
+++ hello/go.mod
@@ -2,9 +2,15 @@ module example/hello
 
 go 1.23.1
 
-require rsc.io/quote v1.5.2
+require (
+	example.com/greetings v0.0.0-00010101000000-000000000000
+	rsc.io/quote v1.5.2
+)
 
 require (
+	golang.org/x/exp v0.0.0-20241009180824-f66d83c29e7c // indirect
 	golang.org/x/text v0.0.0-20170915032832-14c0d48ead0c // indirect
 	rsc.io/sampler v1.3.0 // indirect
 )
+
+replace example.com/greetings => ../greetings
--- hello/hello.go
+++ hello/hello.go
@@ -1,9 +1,38 @@
 package main
 
-import "fmt"
-import "rsc.io/quote"
+import (
+	"fmt"
+	"log"
+
+	// For production use, you’d publish the example.com/greetings module from its repository
+	// (with a module path that reflected its published location), where Go tools could find it to download it
+	"example.com/greetings"
+	"rsc.io/quote"
+)
 
 func main() {
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// Request a greeting message.
+	// message1, err1 := greetings.Hello("")
+	// If an error was returned, print it to the console and
+	// exit the program.
+	// if err1 != nil {
+	// 	log.Fatal(err1)
+	// }
+
+	// If no error was returned, print the returned message
+	// to the console.
+
 	fmt.Println("Hello world!")
 	fmt.Println(quote.Go())
+	message2, err2 := greetings.Hello("Gladys")
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+	fmt.Println(message2)
 }
